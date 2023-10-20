ce023efaa4c7976834afd34c4e7e561ac89ae339
rm98525 <rm98525@fiap.com.br>
2023-10-19 22:30:19 -0300

    global

--- src/app/componentes/cabecalho/cabecalho.jsx
+++ src/app/componentes/cabecalho/cabecalho.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <div><ul>
-    <li><Link href="/produtos/tenis">tenis</Link></li>
-    <li><Link href="/produtos/calca">calca</Link></li>
-    <li><Link href="/produtos/camiseta">camiseta</Link></li>
-    
-    </ul>
-    </div>
-  )
+    <header className="bg-slate-800 text-white">
+      <h1 className="text-center text-3xl font-extrabold">PRODUTOS</h1>
+      <hr className="bg-red-600 border border-red-700"/> 
+      <nav className="menu">
+        <ul>
+          <li>
+            <Link href="/produtos/calca">CALÇA</Link>
+          </li>
+          <li>
+            <Link href="/produtos/camisa">CAMISA</Link>
+          </li>
+          <li>
+            <Link href="/produtos/tenis">TÊNIS</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-
-
-
